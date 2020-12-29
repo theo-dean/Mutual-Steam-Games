9bba57e86f314a08aa10e5cba52afaf691b6f169
Theo Dean <39389891+theo-dean@users.noreply.github.com>
2020-12-29 22:10:28 +0000

    Working response from steam api.
    TODO: Parse response

--- src/SteamUserService.ts
+++ src/SteamUserService.ts
@@ -1,24 +1,39 @@
-class SteamUserService{
-    private API_KEY: string;
+import axios from "axios";
+
+class SteamUserService {
     private API_KEY_PATH = ".key";
-    
+    API_KEY = "";
 
-    SteamUserService(){
-        this.API_KEY = this.loadKey();
-    }
-
-    loadKey(): string{
+    loadKey(): string {
         var fs = require("fs");
         var key = "";
-        fs.readFile(this.API_KEY_PATH, function(err: Error, data: Promise<any>) {
+        // @ts-ignore
+        fs.readFile(this.API_KEY_PATH, (err, data) => {
             if (err) throw err;
-            key = data.toString();
+            this.API_KEY = data.toString();
         });
         return key;
     }
 
-    printKey(){
-        console.log(this.API_KEY);
+    getKey() {
+        return (this.API_KEY);
+    }
+
+    async getGames(steamID: string) {
+        const url = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=" + this.getKey() + "&steamid=" + steamID + "&format=json";
+        try {
+            const response = await axios.get(url);
+            return response.data;       // TODO: Parse response
+        } catch (error){
+            console.error(error);
+        }
+
+    }
+
+    parseJSON(json: string){
+
     }
 }
+
+export {SteamUserService};
 
--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,20 @@
 import express from 'express';
+import { SteamUserService } from './SteamUserService';
+import {TESTING} from "./TESTING";
 
 const app = express();
 const steam = new SteamUserService();
+steam.loadKey();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send(steam.printKey());
+    /*steam.loadKey();
+    let response = steam.getGames("76561198019038761");
+    response.then(data => res.send(data));*/
+    TESTING.jsonPrint();    //TODO: Parse JSON response from SteamUserService getGames()
+
 });
 
-app.listen(port, err => {
-    if (err) {
-        return console.error(err);
-    }
-    return console.log('listening on port:'+port);
-});
+app.listen(port, () => {
+    console.log(`Listening on port:${port}`)
+})

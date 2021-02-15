1bce3eee1bcb14f59909072b30280c745f6ecc43
Theo Dean <39389891+theo-dean@users.noreply.github.com>
2021-02-15 22:21:30 +0000

    fixed parseGame

--- src/DTOParser.ts
+++ src/DTOParser.ts
@@ -2,23 +2,17 @@ import { SteamGame } from "./SteamGame";
 import { SteamUser } from "./SteamUser";
 
 export class DTOParser {
-  static parseUser(
-    steamid: string,
-    response: SteamUserDTO.IRootObject
-  ): SteamUser {
-    let games = [];
+    static parseUser(steamid: string, response: SteamUserDTO.IRootObject): SteamUser {
+        let games = [];
 
-    for (let game of response.response.games) {
-      games.push(game.appid);
+        for (let game of response.response.games) {
+            games.push(game.appid)
+        }
+
+        return new SteamUser(steamid, games);
     }
 
-    return new SteamUser(steamid, games);
-  }
-
-  static parseGame(
-    appid: string,
-    gameResponse: SteamGameDTO.IRootObject
-  ): SteamGame {
-    return new SteamGame(appid, gameResponse.data.name);
-  }
+    static parseGame(appid: string, gameResponse: SteamGameDTO.IRootObject): SteamGame {
+        return new SteamGame(appid, gameResponse.data.name);
+    }
 }

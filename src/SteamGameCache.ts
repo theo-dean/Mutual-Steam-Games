91ac7b2a221006a1e230c89fcd7b8d3a5ac3558c
Theo Dean <39389891+theo-dean@users.noreply.github.com>
2021-02-18 21:53:51 +0000

    worked on cache implementation

--- src/SteamGameCache.ts
+++ src/SteamGameCache.ts
@@ -3,6 +3,7 @@ import { SteamGame } from "./SteamGame";
 export class SteamGameCache {
     private static instance: SteamGameCache;
     private cache: {game: SteamGame, expiryDate: Date}[] = []
+    private CACHE_EXPIRY_LENGTH_MONTHS = 1;  
 
     constructor() {
         if (!!SteamGameCache.instance){
@@ -13,14 +14,34 @@ export class SteamGameCache {
         return this;
     }
 
+    public getGame(appid: string): SteamGame {
+        
+    }
+
     public set(game: SteamGame): void {
-        if (!this.recordExists(game)){
+        if (!this.recordExists(game.appid)){
             let expiryDate = new Date();
-            expiryDate.setMonth(expiryDate.getMonth()+1)
+            expiryDate.setMonth(expiryDate.getMonth()+this.CACHE_EXPIRY_LENGTH_MONTHS)
             this.cache.push({
                 game,
                 expiryDate
             });
         }
+    }
+
+    private recordExists(appid: string): boolean {
+        return !!this.get(appid);
+    }
+
+    private get(appid: string): {game: SteamGame, expiryDate: Date} | undefined{
+        return this.cache.find(x => x.game.appid === appid)
+    }
+
+    private isExpired(): boolean {
+
+    }
+
+    private removeRecord(): void {
+
     }
 }

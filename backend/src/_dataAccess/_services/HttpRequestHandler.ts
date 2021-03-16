354f257b52c594db62a2698fafaa4f1b5fde9650
Theo Dean <39389891+theo-dean@users.noreply.github.com>
2021-03-16 20:50:06 +0000

    Worked on HttpRequestHandler

--- backend/src/_dataAccess/_services/HttpRequestHandler.ts
+++ backend/src/_dataAccess/_services/HttpRequestHandler.ts
@@ -1,5 +1,14 @@
-class HttpRequestHandler {
-    static handle(request: IHttpRequest){
+import axios, {AxiosResponse} from "axios";
 
+class HttpRequestHandler {
+    static async getResponse<T>(request: IHttpRequest): Promise<AxiosResponse<T>>{
+        const {
+            URL: url,
+            Method: method,
+            Headers: headers,
+            Body: data
+        } = request;
+        const response = await axios.request<T>({url, method, headers, data})
+        return response;
     }
 }

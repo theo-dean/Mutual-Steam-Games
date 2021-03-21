ee3f83576f23e0d6bd7d0df8889569a10ce013fc
Theo Dean <39389891+theo-dean@users.noreply.github.com>
2021-03-21 21:00:39 +0000

    added MSW for testing

--- backend/tests/_requests/HttpRequest.i.spec.ts
+++ backend/tests/_requests/HttpRequest.i.spec.ts
@@ -1,13 +1,23 @@
-import { rest } from 'msw';
+import {rest, setupWorker} from 'msw';
 import { setupServer} from "msw/node";
 import {Handlers} from "../../src/_domain/mocks/Handlers";
+import axios from "axios";
 
-const server = setupServer({Handlers});
+const server = setupServer(
+    rest.get('https://store.steampowered.com/api/appdetails', ((req, res, context) =>{
+        return res(
+            context.status(200)
+        )
+    }))
+)
+beforeAll(() => {server.listen()});
+afterAll(()=>{server.close()});
 
 test("Is jest working?", () => {
     expect(true).toBe(true)
 })
 
-test('Is MSW working?', () => {
-
+test('Is MSW working?', async () => {
+    const response = await axios.get("https://store.steampowered.com/api/appdetails");
+    expect(response.status).toBe(200);
 })
